1730e476c878d447a13182c6a2537a3cdc801743
ArteneR <georgevici.ioan@yahoo.com>
2016-05-21 13:05:00 +0300

    Ready to start

--- ScoringLoyality/javascript/form-data.js
+++ ScoringLoyality/javascript/form-data.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
     $("#test").css("background-color", "red");
     
-    
+    $("#button-send").click(function() {
+        
+            var cardHolderName = $("#card-holder-name").val();
+            var cardNumber = $("#card-number").val();
+            var cardExpiryMonth = $("#card-expiry-month").val();
+            var cardExpiryYear = $("#card-expiry-year option:selected").text();
+            var cardCVV = $("#card-cvv").val();
+            var registerToLoyality = $('#register-loyality-check').is(":checked");
+
+            var clientInfo = {
+                    cardHolderName: cardHolderName,
+                    cardNumber: cardNumber,
+                    cardExpiryMonth: cardExpiryMonth,
+                    cardExpiryYear: cardExpiryYear,
+                    cardCVV: cardCVV,
+                    registerToLoyality: registerToLoyality
+            };
+
+            $.ajax({
+                    url: "ScoringLoyality/php/make-payment.php",
+                    data: clientInfo,
+                    method: "POST",
+                    success: function(data) {
+                            alert("success!");
+                    }
+            });
+        
+    });
     
     
     
--- config.php
+++ config.php
@@ -5,3 +5,4 @@ define("LOYALITY_INDICATOR_PATH", "LoyalityIndicator/");
 
 define("JS_PATH", SCORING_LOYALITY_PATH."javascript/");
 define("CSS_PATH", SCORING_LOYALITY_PATH."css/");
+define("PHP_PATH", SCORING_LOYALITY_PATH."php/");
