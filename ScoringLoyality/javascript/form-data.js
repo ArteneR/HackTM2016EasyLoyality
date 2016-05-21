f90223e30e09080625879af720e10bcc84bbb7a2
ArteneR <georgevici.ioan@yahoo.com>
2016-05-21 10:45:43 +0300

    Second commit

---
+++ ScoringLoyality/javascript/form-data.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("#test").css("background-color", "red");
+    
+});
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("SCORING_LOYALITY_PATH", "ScoringLoyality/");
+define("LOYALITY_INDICATOR_PATH", "LoyalityIndicator/");
+
+define("JS_PATH", SCORTING_LOYALITY_PATH."javascript/");
